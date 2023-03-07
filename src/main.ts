edc76d4575db5ac4ac56b4d50c1449ef1ab9895f
larserbach <larserbach@users.noreply.github.com>
2023-03-07 22:54:23 +0100

    works

--- src/main.ts
+++ src/main.ts
@@ -1,47 +1,95 @@
-const sel = figma.currentPage.selection;
+const types = [
+	"BOOLEAN_OPERATION",
+	"COMPONENT",
+	"COMPONENT_SET",
+	"CONNECTOR",
+	"DOCUMENT",
+	"ELLIPSE",
+	"FRAME",
+	"GROUP",
+	"INSTANCE",
+	"LINE",
+	"PAGE",
+	"POLYGON",
+	"RECTANGLE",
+	"SHAPE_WITH_TEXT",
+	"SLICE",
+	"STAMP",
+	"STAR",
+	"STICKY",
+	"TEXT",
+	"VECTOR",
+  ];
+  
 
-const killAll = true
-const doNotKill: string[] = [] 
+// The 'input' event listens for text change in the Quick Actions box after a plugin is 'Tabbed' into.
+figma.parameters.on('input', ({ key, query, result }) => {
+	switch (key) {
+		case 'choice':
 
-kill(sel, doNotKill, killAll)
+			const choices = ["everything", "only nested layers"]
 
-
-function kill(_sel : readonly SceneNode[] | SceneNode[] , _excludes: string[], _killAll = false){
-
-	function removeStyle(elem: any){
-		if(elem.fills != undefined ){
-			elem.fills = [];
-			elem.fillStyleId = "";
-			}
-			if(elem.strokes != undefined ){
-			elem.strokes = [];
-			elem.strokeStyleId = "";
-			}
-
-			if(elem.children && elem.children.length > 0 && elem.type !== 'TEXT'){
-				console.log(elem.type + " - " + elem.name)
-				console.log("has children")
-				return elem.children
-			}
+			const suggestions = choices.filter(s => s.includes(query));
+			result.setSuggestions(suggestions);
+			break;
+		default:
+			return;
 	}
-		
-	_sel.forEach( s => {
-		var children : SceneNode[] = []
+});
 
 
-		if( !_excludes){
-			 children = removeStyle(s)
-		}else{
-			if( !_excludes.includes(s.name) ){
 
-				children = removeStyle(s)
-		}}
-		
-		if(children && children.length > 0){
-			kill(children, _excludes, _killAll)
-		}	
-		
-	})
+// When the user presses Enter after inputting all parameters, the 'run' event is fired.
+figma.on('run', ({ parameters }) => {
+	startPluginWithParameters(parameters!);
+});
+  
 
+function startPluginWithParameters(parameters: any) {
+	console.log(parameters);
+	
+	const sel = figma.currentPage.selection;
+	const excludedNodes: string[] = ["Frame"];
+
+	const nodes =  sel.flatMap(s => getAllNestedNodes(s, excludedNodes))
+	nodes.forEach(removeStyle)
+
+	if(parameters.choice === "everything"){
+		sel.flatMap(s => removeStyle(s))
+	}
+	const message = sel.length > 0 ? "You've got no styles no more" : "🥴 Ooops! Please make a selection"
+	figma.closePlugin(message);
 }
-figma.closePlugin("You've got no styles no more")
+
+
+
+function getAllNestedNodes (sel : SceneNode, excludedNodes: string[]): SceneNode[]
+{
+	
+	const lowerCaseExcludedNodes: string[] = excludedNodes.map(n => n.toLowerCase());
+	console.log(lowerCaseExcludedNodes)
+
+	const filter = (node: any) => !lowerCaseExcludedNodes.includes(node.name.toLowerCase());
+	
+	const nodes  = ('children' in sel) ? (sel.findAll( (node: SceneNode) => excludedNodes.length === 0 ? true : filter(node))) : [];
+
+	return nodes;
+	
+};
+
+
+
+
+function removeStyle(elem: any){
+	
+	if(elem.fills != undefined ){
+		elem.fills = [];
+		elem.fillStyleId = "";
+		console.log("remove fills")
+	}
+
+	if(elem.strokes != undefined ){
+		elem.strokes = [];
+		elem.strokeStyleId = "";
+	}
+}
