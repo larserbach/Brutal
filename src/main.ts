31f88f5f404edcebdf3d7c4c1466e0074ee17a46
larserbach <larserbach@users.noreply.github.com>
2023-04-12 17:28:39 +0200

    inbetween

--- src/main.ts
+++ src/main.ts
@@ -75,6 +75,7 @@ const SEMITRANSPARENT: Paint[] = [{
 
 
 // The 'input' event listens for text change in the Quick Actions box after a plugin is 'Tabbed' into.
+
 figma.parameters.on(
 	'input', 
 	({ key, query, result }: ParameterInputEvent) => {
@@ -82,17 +83,20 @@ figma.parameters.on(
 		switch (key) {
 			case 'choice':
 				// the user must choose if he want's to remove only nested layers
-				const choices = ["All layers in selection", "Only nested layers in selection"]
-				const suggestions = choices.filter(s => s.includes(query));
-				result.setSuggestions(suggestions);
+				const choiceOptions = ["All layers in selection", "Only nested layers in selection"]
+				const choiceSuggestions = choiceOptions.filter(s => s.includes(query));
+				result.setSuggestions(choiceSuggestions);
 			break;
 
 			case 'name':
-			// providing the option to preserve styles on named layers
-	
-			const namesuggestions = listExcludableNodes(query);
-			result.setSuggestions(namesuggestions);
-				
+				const nameSuggestions = listExcludableNodes(query);
+				result.setSuggestions(nameSuggestions);			
+			break;
+
+			case 'settings_excludeChildren':
+				const includeChildrenOptions = ["Exclude only named layer(s)", "Also exclude children of named layer(s)"]
+				const includeChildrenSuggestions = includeChildrenOptions.filter(s => s.includes(query));
+				result.setSuggestions(includeChildrenSuggestions);	
 			break;
 
 			default:
@@ -102,13 +106,13 @@ figma.parameters.on(
 
 function listExcludableNodes(query: string): any {
 
-	
-	const sel : readonly SceneNode[] = figma.currentPage.selection;
-	
-	const customfilter = (node: SceneNode) =>  query.length === 0 ? true : node.name.toLowerCase().startsWith(query.toLowerCase());
-	
+	// users selection
+	const sel : readonly SceneNode[] = figma.currentPage.selection;	
+	// the filter
+	const inputfilter = (node: SceneNode) =>  query.length === 0 ? true : node.name.toLowerCase().startsWith(query.toLowerCase());
+	// Set to collect all valid node names
 	let namesSet: Set<string> = new Set()
-	
+	// Worker function to get all supported child nodes
 	const filteredChildNodes = (node : SceneNode) : SceneNode[] =>
 	{
 		return ('children' in node) ? 
@@ -116,29 +120,23 @@ function listExcludableNodes(query: string): any {
 				// If node is not of supported type eg:(SLICE), it will not be added
 				if(!supportedNodes(child)) return false
 				// Only nodes with names matching the user input
-				return child.name != null && customfilter(child);
+				return child.name != null && inputfilter(child);
 			}) : [];
 	}
-
+	// Worker function to get all supported nodes in selection
 	const filteredSelection = (nodes: readonly SceneNode[]) : SceneNode [] => 
 	{
 		return nodes.filter( (node: SceneNode) => 
 			supportedNodes(node)).
-			filter(node => node.name != null && customfilter(node))
+			filter(node => node.name != null && inputfilter(node))
 	}
 
-	
+	// add names of valid child nodes to namesSet
 	sel.flatMap((selectedNode) => filteredChildNodes(selectedNode).forEach(node => {namesSet.add(node.name)}));
-	
+	// add names of valid selected nodes to namesSet
 	filteredSelection(sel).forEach((node: SceneNode) => namesSet.add(node.name))
 	
-	
-	console.log(namesSet.size)
-	
-	for (const item of namesSet) {
-		console.log(item);
-	}
-	
+	// create an array from namesSet
 	const formattedNodes = Array.from(namesSet, (name) => ({ name, data: name }));
 	const namesuggestions = [...formattedNodes];
 	return namesuggestions
@@ -157,7 +155,7 @@ figma.on('run', ({ parameters }) => {
 	}
 });
   
-
+let validNodes: SupportedNode[] = []
 // Manages logic depending on user input, returns messages when everything is done
 function startPluginWithParameters(parameters: any, command: string): string {
 
@@ -172,20 +170,43 @@ function startPluginWithParameters(parameters: any, command: string): string {
 	// Nodes with this string will not be touched
 	const excludedNode: string = parameters.name ? parameters.name.toLowerCase() : [];
 
+	// setting validNodes
+	if(parameters.choice === "Only nested layers in selection"){
 		
-	// Collect all valid child nodes in 'nodes' array
-	let nodes: SupportedNode[] = sel.flatMap(node => getAllValidNestedNodes(node, excludedNode))
+		sel.forEach( node => {
+			if(!supportedNodes(node)) return;
 	
-	// If the user choose to remove styles from any layer, add valid selected nodes to 'nodes' array
-	if(parameters.choice === "All layers in selection"){
-	
-		const filteredSel: SupportedNode[] = sel.filter( (node: SceneNode) : boolean => {
-			if(!supportedNodes(node)) return false
-			return excludedNode.length === 0 ? true : !node.name.toLowerCase().startsWith(excludedNode)
-			}) as SupportedNode[];
+			if(excludedNode.length === 0 ? true : !node.name.toLowerCase().startsWith(excludedNode)){
+				if('children' in node && node.children.length > 0){
+					const children = node.children
+					setValidNodesRecursivly(children, excludedNode)
+				}
+			}
+		})
 
-		filteredSel.forEach(node => nodes.push(node))
+	} else if (parameters.choice === "All layers in selection"){
+		setValidNodesRecursivly(sel, excludedNode)
+	} else {
+		throw new Error('some issue with parameters.choice')
 	}
+	
+	// getting validNodes
+	const nodes = validNodes as SupportedNode[];
+	
+	// console.log(nodes)
+	// Collect all valid child nodes in 'nodes' array
+	// let nodes: SupportedNode[] = sel.flatMap(node => getAllValidNestedNodes(node, excludedNode))
+	
+	// // If the user choose to remove styles from any layer, add valid selected nodes to 'nodes' array
+	// if(parameters.choice === "All layers in selection"){
+	
+	// 	const filteredSel: SupportedNode[] = sel.filter( (node: SceneNode) : boolean => {
+	// 		if(!supportedNodes(node)) return false
+	// 		return excludedNode.length === 0 ? true : !node.name.toLowerCase().startsWith(excludedNode)
+	// 		}) as SupportedNode[];
+
+	// 	filteredSel.forEach(node => nodes.push(node))
+	// }
 
 	// Depending on the menue command we proceed with removing or replacing styles on all collected nodes
 	switch (command) {
@@ -194,7 +215,7 @@ function startPluginWithParameters(parameters: any, command: string): string {
 			return "👻 Styles removed"
 		case CMD_REPLACE_STYLES:
 			nodes.forEach(replaceStyle)		
-			return "✨ Layers got a new paintjob"
+			return "✨ Styles replaced"
 		default:
 			throw new Error('Plugin started with unknown command: ' + JSON.stringify(command))
 	}
@@ -204,6 +225,38 @@ function startPluginWithParameters(parameters: any, command: string): string {
 /* # # # FUNCTIONS # # # #*/
 /* # # # # # # # # # # # #*/
 
+
+function setValidNodesRecursivelyXX(nodes: SceneNode[] | readonly SceneNode[], excludedNode: string = "", excludeChildren = false) {
+	nodes.forEach(node => {
+	  if ('children' in node && node.children.length > 0 && !excludeChildren) {
+		setValidNodesRecursivelyXX(node.children, excludedNode, excludeChildren);
+	  }
+	  if (excludedNode.length !== 0 && node.name.toLowerCase().startsWith(excludedNode) || !supportedNodes(node)) {
+		return;
+	  } else {
+		validNodes.push(node);
+	  }
+	});
+  }
+
+  
+
+
+function setValidNodesRecursivly (nodes : SceneNode[] | readonly SceneNode[], excludedNode: string = "", excludeChildren = false)
+{
+	
+	nodes.forEach( node => {
+		
+		if(excludedNode.length === 0 ? true : !node.name.toLowerCase().startsWith(excludedNode)){
+			if( 'children' in node && node.children.length > 0){
+				const children = node.children
+				setValidNodesRecursivly(children, excludedNode)
+			}
+			if(!supportedNodes(node)) return;
+			validNodes.push(node)
+		}
+	})
+}
 
 // Returns childnodes of any node passed in to it. Children get filtered by type and name
 function getAllValidNestedNodes (sel : SceneNode, excludedNode: string = ""): SupportedNode[]
