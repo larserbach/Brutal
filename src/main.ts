14b813918fa00600a8eca8f0cac8afede4010777
larserbach <larserbach@users.noreply.github.com>
2023-12-17 14:48:08 +0100

    adds granular functions

--- src/main.ts
+++ src/main.ts
@@ -86,7 +86,10 @@ figma.parameters.on("input", ({ key, query, result }: ParameterInputEvent) => {
   switch (key) {
     case "choice":
       // the user must choose if he want's to remove only nested layers
-      const choiceOptions = [PARAM_All_LAYERS_TRUE, PARAM_All_LAYERS_FALSE];
+      const choiceOptions = [
+        PARAM_All_LAYERS_TRUE, 
+        PARAM_All_LAYERS_FALSE
+      ];
       const choiceSuggestions = choiceOptions.filter((s) => s.includes(query));
       result.setSuggestions(choiceSuggestions);
       break;
@@ -210,15 +213,15 @@ function startPluginWithParameters(parameters: any, command: string): string {
 
 
   // getting validNodes
-  const nodes = validNodes as SupportedNode[];
+  // const nodes = validNodes as SupportedNode[];
 
   // Depending on the menue command we proceed with removing or replacing styles on all collected nodes
   switch (command) {
     case CMD_REMOVE_STYLES:
-      nodes.forEach(removeStyle);
+      validNodes.forEach(removeStyle);
       return "👻 Styles removed";
     case CMD_REPLACE_STYLES:
-      nodes.forEach(replaceStyle);
+      validNodes.forEach(replaceStyle);
       return "✨ Styles replaced";
     default:
       throw new Error(
@@ -265,6 +268,392 @@ function setValidNodesRecursively(
   });
 }
 
+
+
+function clone(val:any) : any  {
+  const type = typeof val
+  if (val === null) {
+    return null
+  } else if (type === 'undefined' || type === 'number' ||
+             type === 'string' || type === 'boolean') {
+    return val
+  } else if (type === 'object') {
+    if (val instanceof Array) {
+      return val.map(x => clone(x))
+    } else if (val instanceof Uint8Array) {
+      return new Uint8Array(val)
+    } else {
+      let o:any = {}
+      for (const key in val) {
+        o[key] = clone((val)[key])
+      }
+      return o
+    }
+  }
+  throw 'unknown'
+}
+
+
+// FILLS
+
+function removeFillStyle (node:SupportedNode):void {
+  if (node.fillStyleId === ""){
+    console.log('this node has no fill style')
+    return
+  }
+
+  console.log('This node contains a fill style')
+  console.log('detaching style')
+  node.fillStyleId = ""
+  console.log('removing all fills')
+  node.fills = []
+}
+
+function detachFillStyle (node:SupportedNode):void {
+  if (node.fillStyleId === ""){
+    console.log('this node has no fill style')
+    return
+  }
+
+  console.log('This node contains a fill style')
+  console.log('detaching style')
+  node.fillStyleId = ""
+}
+
+function detachFillVariables (node:SupportedNode): void {
+  if (node.fillStyleId !== ""){
+    console.log('this node has a style applied. I will not touch it')
+    return
+  }
+
+  console.log('will check for variables')
+  // Checking if it's an array
+  if (!(node.fills instanceof Array)) {return}
+  // Use type assertion to inform TypeScript that node.fills is an array
+  var clonedFills = (node.fills as any[]).map(x => clone(x))
+
+  node.fills = clonedFills.map(fill => {
+    const hasVariableAlias = fill?.boundVariables?.color?.type === "VARIABLE_ALIAS";
+    if (hasVariableAlias) {
+      console.log('this is a fill with a bound variable');
+      console.log('detaching now')
+      fill.boundVariables = {}
+    } 
+    return fill
+  })
+}
+
+function removeFillVariables (node:SupportedNode): void {
+  if (node.fillStyleId !== ""){
+    console.log('this node has a style applied. I will not touch it')
+    return
+  }
+
+  console.log('will check for variables')
+  // Checking if it's an array
+  if (!(node.fills instanceof Array)) {return}
+  // Use type assertion to inform TypeScript that node.fills is an array
+  var clonedFills = (node.fills as any[]).map(x => clone(x))
+
+  var newfills: any[] = []
+
+  clonedFills.forEach(fill => {
+    const hasVariableAlias = fill?.boundVariables?.color?.type === "VARIABLE_ALIAS";
+    if (!hasVariableAlias) {
+      return newfills.push(fill)
+    } 
+  })
+  console.log (newfills)
+  node.fills = newfills
+}
+
+function removeCustomFills (node:SupportedNode): void {
+  if (node.fillStyleId !== ""){
+    console.log('this node has a style applied. I will not touch it')
+    return
+  }
+
+  console.log('will check for variables')
+  // Checking if it's an array
+  if (!(node.fills instanceof Array)){return}
+  // Use type assertion to inform TypeScript that node.fills is an array
+  var clonedFills = (node.fills as any[]).map(x => clone(x))
+
+  var newfills: any[] = []
+
+  clonedFills.forEach(fill => {
+    const hasVariableAlias = fill?.boundVariables?.color?.type === "VARIABLE_ALIAS";
+    if (hasVariableAlias) {
+      return newfills.push(fill)
+    } 
+  })
+  console.log (newfills)
+  node.fills = newfills
+}
+
+function removeAnyFill (node:SupportedNode){
+  node.fills = [];
+  node.fillStyleId = "";
+}
+// STROKES 
+
+function removeStrokeStyle (node:SupportedNode):void {
+  // guard
+  if (node.type === "SECTION") return;
+
+  if (node.strokeStyleId === ""){
+    console.log('this node has no stroke style')
+    return
+  }
+
+  console.log('This node contains a stroke style')
+  console.log('detaching stroke style')
+  node.strokeStyleId = ""
+  console.log('removing all strokes')
+  node.strokes = []
+}
+
+function detachStrokeStyle (node:SupportedNode):void {
+  // guard
+  if (node.type === "SECTION") return;
+
+  if (node.strokeStyleId === ""){
+    console.log('this node has no stroke style')
+    return
+  }
+
+  console.log('This node contains a stroke style')
+  console.log('detaching stroke style')
+  node.strokeStyleId = ""
+}
+
+function detachStrokeVariables (node:SupportedNode): void {
+  
+  // guard
+  if (node.type === "SECTION") return;
+
+  if (node.strokeStyleId !== ""){
+    console.log('this node has a stroke style applied. I will not touch it')
+    return
+  }
+
+  console.log('will check for variables')
+  // Checking if it's an array
+  if (!(node.strokes instanceof Array)) {return}
+
+  // Use type assertion to inform TypeScript that node.fills is an array
+  var clonedStrokes = (node.strokes as any[]).map(x => clone(x))
+  console.log(clonedStrokes)
+
+  node.strokes = clonedStrokes.map(stroke => {
+    console.log(stroke)
+    const hasVariableAlias = stroke?.boundVariables?.color?.type === "VARIABLE_ALIAS";
+    if (hasVariableAlias) {
+      console.log('this is a stroke with a bound variable');
+      console.log('detaching now')
+      stroke.boundVariables = {}
+    } 
+    return stroke
+  })
+}
+
+function removeStrokeVariables (node:SupportedNode): void {
+
+  // guard
+  if (node.type === "SECTION") return;
+  
+  if (node.strokeStyleId !== ""){
+    console.log('this node has a stroke style applied. I will not touch it')
+    return
+  }
+
+  console.log('will check for variables')
+  // Checking if it's an array
+  if (!(node.strokes instanceof Array)) {return}
+  // Use type assertion to inform TypeScript that node.strokes is an array
+  var clonedStrokes = (node.strokes as any[]).map(x => clone(x))
+
+  var newstrokes: any[] = []
+
+  clonedStrokes.forEach(stroke => {
+    const hasVariableAlias = stroke?.boundVariables?.color?.type === "VARIABLE_ALIAS";
+    if (!hasVariableAlias) {
+      return newstrokes.push(stroke)
+    } 
+  })
+  console.log (newstrokes)
+  node.strokes = newstrokes
+}
+
+function removeCustomStrokes (node:SupportedNode): void {
+
+  // guard
+  if (node.type === "SECTION") return;
+
+  if (node.strokeStyleId !== ""){
+    console.log('this node has a style applied. I will not touch it')
+    return
+  }
+
+  console.log('will check for variables')
+  // Checking if it's an array
+  if (!(node.strokes instanceof Array)){return}
+  // Use type assertion to inform TypeScript that node.strokes is an array
+  var clonedStrokes = (node.strokes as any[]).map(x => clone(x))
+
+  var newstrokes: any[] = []
+
+  clonedStrokes.forEach(stroke => {
+    const hasVariableAlias = stroke?.boundVariables?.color?.type === "VARIABLE_ALIAS";
+    if (hasVariableAlias) {
+      return newstrokes.push(stroke)
+    } 
+  })
+  console.log (newstrokes)
+  node.strokes = newstrokes
+}
+
+function removeAnyStoke (node:SupportedNode){
+  // guard
+  if (node.type === "SECTION") return;
+
+  node.strokes = [];
+  node.strokeStyleId = "";
+}
+
+// EFFECTS
+
+function removeEffectStyle (node:SupportedNode):void {
+  // guard
+  if (node.type === "SECTION" || node.type === "SHAPE_WITH_TEXT") return;
+
+
+  if (node.effectStyleId === ""){
+    console.log('this node has no effect style')
+    return
+  }
+
+  console.log('This node contains a effect style')
+  console.log('detaching effect style')
+  node.effectStyleId = ""
+  console.log('removing all effects')
+  node.effects = []
+}
+
+function detachEffectStyle (node:SupportedNode):void {
+  // guard
+  if (node.type === "SECTION" || node.type === "SHAPE_WITH_TEXT") return;
+
+  if (node.effectStyleId === ""){
+    console.log('this node has no effect style')
+    return
+  }
+
+  console.log('This node contains a effect style')
+  console.log('detaching effect style')
+  node.effectStyleId = ""
+}
+
+function detachEffectVariables (node:SupportedNode): void {
+  // guard
+  if (node.type === "SECTION" || node.type === "SHAPE_WITH_TEXT") return;
+
+  if (node.effectStyleId !== ""){
+    console.log('this node has a effect style applied. I will not touch it')
+    return
+  }
+
+  console.log('will check for variables')
+  // Checking if it's an array
+  if (!(node.effects instanceof Array)) {return}
+
+  // Use type assertion to inform TypeScript that node.fills is an array
+  var clonedEffects = (node.effects as any[]).map(x => clone(x))
+  console.log(clonedEffects)
+
+  node.effects = clonedEffects.map(effect => {
+    console.log(effect)
+    const hasVariableAlias = effect?.boundVariables?.color?.type === "VARIABLE_ALIAS";
+    if (hasVariableAlias) {
+      console.log('this is a effect with a bound variable');
+      console.log('detaching now')
+      effect.boundVariables = {}
+    } 
+    return effect
+  })
+}
+
+function removeEffectVariables (node:SupportedNode): void {
+  // guard
+  if (node.type === "SECTION" || node.type === "SHAPE_WITH_TEXT") return;
+  
+  if (node.effectStyleId !== ""){
+    console.log('this node has a effect style applied. I will not touch it')
+    return
+  }
+
+  console.log('will check for variables')
+  // Checking if it's an array
+  if (!(node.effects instanceof Array)) {return}
+  // Use type assertion to inform TypeScript that node.effects is an array
+  var clonedEffects = (node.effects as any[]).map(x => clone(x))
+
+  var neweffects: any[] = []
+
+  clonedEffects.forEach(effect => {
+    const hasVariableAlias = effect?.boundVariables?.color?.type === "VARIABLE_ALIAS";
+    if (!hasVariableAlias) {
+      return neweffects.push(effect)
+    } 
+  })
+  console.log (neweffects)
+  node.effects = neweffects
+}
+
+function removeCustomEffects (node:SupportedNode): void {
+  // guard
+  if (node.type === "SECTION" || node.type === "SHAPE_WITH_TEXT") return;
+
+  if (node.effectStyleId !== ""){
+    console.log('this node has a style applied. I will not touch it')
+    return
+  }
+
+  console.log('will check for variables')
+  // Checking if it's an array
+  if (!(node.effects instanceof Array)){return}
+  // Use type assertion to inform TypeScript that node.effects is an array
+  var clonedEffects = (node.effects as any[]).map(x => clone(x))
+
+  var neweffects: any[] = []
+
+  clonedEffects.forEach(effect => {
+    const hasVariableAlias = effect?.boundVariables?.color?.type === "VARIABLE_ALIAS";
+    if (hasVariableAlias) {
+      return neweffects.push(effect)
+    } 
+  })
+  console.log (neweffects)
+  node.effects = neweffects
+}
+
+function removeAnyEffect (node:SupportedNode){
+  // guard
+  if (node.type === "SECTION" || node.type === "SHAPE_WITH_TEXT") return;
+
+  node.effects = [];
+  node.effectStyleId = "";
+}
+
+
+
+// Removes fills, effects and strokes of single node
+function removeStyle(node: SupportedNode) {
+  //removeCustomFills(node)
+  removeAnyEffect(node)
+
+}
+
 // Replaces fills and strokes of single node with predefined colors SEMITRANSPARENT / OPAQUE
 function replaceStyle(elem: SupportedNode) {
   // replace fills
@@ -287,33 +676,6 @@ function replaceStyle(elem: SupportedNode) {
 
   // remove effects
   if (elem.effects != undefined) {
-    elem.effects = [];
-    elem.effectStyleId = "";
-  }
-}
-
-// Removes fills, effects and strokes of single node
-function removeStyle(elem: SupportedNode) {
-  // remove fills
-  if (elem.fills != undefined) {
-    elem.fills = [];
-    elem.fillStyleId = "";
-  }
-
-  // guard
-  if (elem.type === "SECTION") return;
-
-  // remove strokes
-  if (elem.strokes !== undefined) {
-    elem.strokes = [];
-    elem.strokeStyleId = "";
-  }
-
-  // guard
-  if (elem.type === "SHAPE_WITH_TEXT") return;
-
-  // remove effects
-  if (elem.effects !== undefined) {
     elem.effects = [];
     elem.effectStyleId = "";
   }
